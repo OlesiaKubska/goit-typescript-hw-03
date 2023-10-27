c074236d71d6488533d98099aefc213cef18cf17
OlesiaKubska <kublesia0908@gmail.com>
2023-10-27 23:36:13 +0200

    Update files TypeScript

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+    private signature: number = Math.random();
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Person has entered the house.');
+        } else {
+            console.log('Door is closed. Person cannot enter.');
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Door is opened.');
+        } else {
+            console.log('Wrong key. Door is still closed.');
+        }
+    }
+}
+
+// Сценарій взаємодії
 const key = new Key();
 
 const house = new MyHouse(key);
